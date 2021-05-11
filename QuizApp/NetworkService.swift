9b91b13e12e8020dc20e88322ab2206a8d687276
Marin <marin.h>
2021-05-11 09:02:58 +0200

    NetworkService

---
+++ QuizApp/NetworkService.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkService.swift
+//  QuizApp
+//
+//  Created by Marin on 10.05.2021..
+//
+
+import UIKit
+class NetworkService: NetworkServiceProtocol {
+    
+}
---
+++ QuizApp/NetworkServiceProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkServiceProtocol.swift
+//  QuizApp
+//
+//  Created by Marin on 10.05.2021..
+//
+
+import UIKit
+protocol NetworkServiceProtocol {
+    
+}
